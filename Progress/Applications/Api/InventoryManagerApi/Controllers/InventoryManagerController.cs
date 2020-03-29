ee0c09a07f4f34eecd595d07d8f020630c267429
Jake Young <yjake86@yahoo.com>
2020-03-28 22:22:02 -0500

    Fixed project references

--- Progress/Applications/Api/InventoryManagerApi/Controllers/InventoryManagerController.cs
+++ Progress/Applications/Api/InventoryManagerApi/Controllers/InventoryManagerController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Domain.Product.Business;
 using Domain.Product.DataContract;
 using Microsoft.AspNetCore.Mvc;
